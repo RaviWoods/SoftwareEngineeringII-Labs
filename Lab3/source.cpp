0d75d031e4927686860e97750c5c0d1ac14c002f
Ravi Woods <rbw14@eews304-019.ee.ic.ac.uk>
2015-11-04 10:09:55 +0000

    Added << to point

---
+++ Lab3/source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <math.h>
+#include "point.hpp"
+using namespace std;
+
+int main() {
+	cout << "-------------------" << endl;
+	point p1(2,1);
+	cout << p1 << endl;
+
+}
