69f4b8dafb136e563407aab65ed960afb003d74e
Ravi Woods <rbw14@eews304-018.ee.ic.ac.uk>
2015-11-11 10:55:12 +0000

    Bin search working, take in point from file does not

--- Lab3/source.cpp
+++ Lab3/source.cpp
@@ -1,54 +1,66 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <stdlib.h>
 #include <math.h>
 #include <vector>
 #include "point.hpp"
 using namespace std;
 
-int member_point_unordsearch (const vector<point>& vin, point pin) {
-
+int member_point_unordsearch (const vector<point>& vin, point pin,  bool& found) {
+	
+	found = true;
 	for(int i = 0; i < vin.size(); i++) { 
 		if (vin[i] == pin) { return (i) ; };	
 	}
+	found = false;
 	return vin.size();
 }
 
-int member_point_binsearch (const vector<point>& vin, point pin, int& ) {
-
+int member_point_binsearch (const vector<point>& vin, point pin, bool& found) {
+	found = true;
+	//cout << "input = " << pin << "\t";
 	int lower_bound = 0;
 	int upper_bound = vin.size();
 	int bound_range = vin.size();
 	while(bound_range != 1) {
-		if (vin[floor(bound_range/2)] == pin) {
-			return floor(bound_range/2);
-		} else if (vin[floor(bound_range/2)] < pin) {
-			lower_bound = floor(bound_range/2);
+		
+		if (vin[floor(lower_bound + bound_range/2)] == pin) {
+			
+			//cout << "PASS" << "\t";
+			return lower_bound + floor(bound_range/2);
+		} else if (vin[floor(lower_bound + bound_range/2)] < pin) {
+			lower_bound = lower_bound + floor(bound_range/2);
 		} else {
-			upper_bound = floor(bound_range/2);
+			upper_bound = lower_bound + floor(bound_range/2);
 		}
 		bound_range = upper_bound - lower_bound;
+		//cout << "lb = " << lower_bound << endl;	
+		//cout << "ub = " << upper_bound << endl;
+		
 	}
 	if (vin[lower_bound] == pin) {
+		//cout << "PASS" << "\t";
 		return lower_bound;
 	} else if (vin[upper_bound] == pin) {
+		//cout << "PASS" << "\t";
 		return upper_bound;
 	} else {
+		found = false;
+		//cout << "FAIL" << "\t";
 		return vin.size(); 
 	}
 }
 
-
 int main() {
 	cout << "-------------------" << endl;
-	point p1(1,1);
+	point p1(2,3);
 	vector<point> v1;
-	int a = 0;
-	for (int i = 0; i < 10; i++) {	
-		v1.push_back(point(a,a));
-		a++;
-	}
-	cout << member_point_unordsearch(v1,p1) << endl;
+	vector<point> v2;
+	vector<int> v3;
+
+	
 }
+
 
 
---
+++ Lab4/point.hpp
@@ -0,0 +1,36 @@
+#ifndef POINT_HPP
+#define POINT_HPP
+
+class point {
+public:	
+	point() : x(0), y(0), r(0) {}
+	point(double x_in, double y_in) : x(x_in), y(y_in) {
+		set_r();
+	}
+	~point() {
+		std::cout << *this << std::endl;
+	}
+	void set_x(double x_in);
+	void set_y(double y_in);
+	void set_r();
+
+	double get_x() const;
+	double get_y() const;
+	double get_r() const;
+
+	friend bool operator <(const point& p1, const point& p2);
+	friend bool operator ==(const point& p1, const point& p2);
+	friend std::ostream& operator<<(std::ostream& os, const point& pt);
+	friend std::istream& operator>>(std::istream& is, point& pt);
+	std::string print();
+	double twopointdist(point p2);
+	void sym();
+	void translate(point p2);
+	
+private:
+	double x;
+	double y;
+	double r;
+};
+
+#endif
