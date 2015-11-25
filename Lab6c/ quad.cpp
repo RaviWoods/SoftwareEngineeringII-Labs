bf4010dffae9435751b55b2f3edab514b80eae33
RaviWoods <rbw14@eews304-022.ee.ic.ac.uk>
2015-11-25 10:09:48 +0000

    Lab 6 pg1 fin

---
+++ Lab6c/ quad.cpp
@@ -0,0 +1,29 @@
+#include "../libs.h"
+#include "point.hpp"
+#include "poly.hpp"
+#include "quad.hpp"
+
+using namespace std;
+
+double Quadrilateral::perimeter() {
+	double l1 = p1.twopointdist(p2);
+	double l2 = p2.twopointdist(p3);
+	double l3 = p3.twopointdist(p4);
+	double l4 = p4.twopointdist(p1);
+	return (l1 + l2 + l3 + l4);	
+}
+
+void Quadrilateral::translate(Point vect) {
+	p1.translate(vect);
+	p2.translate(vect);
+	p3.translate(vect);
+	p4.translate(vect);
+}
+
+string Quadrilateral::strmake() const {
+	stringstream ss;
+	ss << "Quadrilateral, with points: " << endl;
+	ss << "p1: " << p1 << " , p2: " << p2 << " , p3: " << p3 << " , p4: " << p4 << endl;
+	return ss.str();
+}
+
--- Lab6c/quad.hpp~
+++ Lab6c/quad.hpp~
@@ -1,12 +1,12 @@
-#ifndef TRI_HPP
-#define TRI_HPP
+#ifndef QUAD_HPP
+#define QUAD_HPP
 #include "../libs.h"
 #include "point.hpp"
 #include "poly.hpp"
 
-class Triangle: public Polygon {
+class Quadrilateral: public Polygon {
 public:
-	Triangle(Point p1_in, Point p2_in, Point p3_in) : p1(p1_in), p2(p2_in) , p3(p3_in) {}
+	Quadrilateral(Point p1_in, Point p2_in, Point p3_in, Point p4_in) : p1(p1_in), p2(p2_in) , p3(p3_in), p4(p4_in) {}
 	double perimeter();
 	void translate(Point vect);
 	std::string strmake() const;
@@ -14,6 +14,7 @@ private:
 	Point p1;
 	Point p2;
 	Point p3;
+	Point p4;
 };
 
 #endif
